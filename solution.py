cf2ea1862af3deeacc3c292324631907cd4e65ca
Vadimmmzz <91953115+Vadimmmzz@users.noreply.github.com>
2023-04-06 22:55:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 230865321 
 
 def solution(x: np.array) -> float:
     x=x+21
     y=np.exp(x)
-    return y.mean() # Ваш ответ
+    return y.mean()/10
