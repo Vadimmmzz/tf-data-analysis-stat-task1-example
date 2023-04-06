30c92bfe6fcfa6ae1a5ba0cc450e419e54853965
Vadimmmzz <91953115+Vadimmmzz@users.noreply.github.com>
2023-04-06 22:51:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x=x+21
+    y=np.exp(x)
+    return y.mean() # Ваш ответ
